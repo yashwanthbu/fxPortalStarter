12d64ca4af1f17d67ed627d97ac4eb51f886beea
yashwanthbu <141665731+yashwanthbu@users.noreply.github.com>
2024-09-18 23:00:33 +0530

    Update mint.js

--- scripts/mint.js
+++ scripts/mint.js
@@ -1,30 +1,21 @@
-// We require the Hardhat Runtime Environment explicitly here. This is optional
-// but useful for running the script in a standalone fashion through `node <script>`.
-//
-// You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
-// will compile your contracts, add the Hardhat Runtime Environment's members to the
-// global scope, and execute the script.
-const hre = require("hardhat");
-const tokenContractJSON = require("../artifacts/contracts/MetaToken.sol/MetaToken.json");
-require('dotenv').config()
-
-const tokenAddress = ""; // place your erc20 contract address here
-const tokenABI = tokenContractJSON.abi;
-const walletAddress = ""; // place your public address for your wallet here
+const { ethers } = require("hardhat");
+require("dotenv").config();
 
 async function main() {
+  const privateKey = process.env.PRIVATE_KEY;
+  const networkAddress = "https://ethereum-sepolia-rpc.publicnode.com";
+  const provider = new ethers.providers.JsonRpcProvider(networkAddress);
+  const signer = new ethers.Wallet(privateKey, provider);
+  const contractAddress = "0xfFF16e281092d3550b890157E433005e98F844C3";
 
-    const token = await hre.ethers.getContractAt(tokenABI, tokenAddress);
-  
-    const tx = await token.mint(walletAddress, 1000);
-    await tx.wait();
-
-    console.log("You now have: " + await token.balanceOf(walletAddress) + " tokens");
-  }
-  
-  // We recommend this pattern to be able to use async/await everywhere
-  // and properly handle errors.
-  main().catch((error) => {
+  const SuperIronBatman = await ethers.getContractFactory("SuperIronBatman", signer);
+  const SuperIronBatman_contract = await SuperIronBatman.attach(contractAddress);
+  await SuperIronBatman_contract.mint(5);
+  console.log("Minted 5 superIronBatman!");
+}
+main()
+  .then(() => process.exit(0))
+  .catch(error => {
     console.error(error);
-    process.exitCode = 1;
+    process.exit(1);
   });
