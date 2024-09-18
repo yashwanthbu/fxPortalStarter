192c63e397abd685da0bf752b43dbc0b92944712
yashwanthbu <141665731+yashwanthbu@users.noreply.github.com>
2024-09-18 22:58:47 +0530

    Update MetaToken.sol

--- contracts/MetaToken.sol
+++ contracts/MetaToken.sol
@@ -1,17 +1,37 @@
 // SPDX-License-Identifier: MIT
+
 pragma solidity ^0.8.9;
 
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
+import "erc721a/contracts/ERC721A.sol";
 
-contract MetaToken is ERC20, Ownable {
-    constructor() ERC20("MetaToken", "MTA") {}
+contract SuperIronBatman is ERC721A{
+    address public owner;
+    uint256 public max = 5;
+    string baseUrl = "https://gateway.pinata.cloud/ipfs/QmXHX7P1XvvzGXcCfJsA4z73Nem2rAe5H3iS79MfR3pVuP";
 
-    function mint(address to, uint256 amount) public onlyOwner {
-        _mint(to, amount);
+    string public prompt =
+        "superman with a batman mask fighting with ironman";
+
+    constructor() ERC721A("SuperBatIronMan", "SBI") {
+        owner = msg.sender;
     }
 
-    function decimals() public pure override returns (uint8) {
-		return 0;
-	}
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Only owner can call this function");
+        _;
+    }
+
+    function mint(uint256 quantity) external payable onlyOwner{
+        require(totalSupply() + quantity <= max ,"Allowed Nfts = 5");
+        _mint(msg.sender, quantity);
+    }
+
+
+    function _baseURI() internal view override returns (string memory){
+        return baseUrl;
+    }
+
+    function promptDescription() external view returns (string memory) {
+        return prompt;
+    }
 }
